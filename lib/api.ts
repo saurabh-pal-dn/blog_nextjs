0617c50c87e7ba0967fdd3116658cb52c5064707
Saurabh Pal <saurabh.pal.dn@gmail.com>
2023-04-13 14:44:56 +0530

    adding getAllPopPosts()

--- lib/api.ts
+++ lib/api.ts
@@ -1,27 +1,34 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import { join } from 'path';
-import { POSTS_PATH } from '../utils/mdxUtils';
-
-export function getPostSlugs(): string[] {
-  return fs.readdirSync(POSTS_PATH);
-}
+import { POSTS_PATH, POP_POSTS_PATH } from '../utils/mdxUtils';
 
 type PostItems = {
   [key: string]: string;
 };
 
-export function getPostBySlug(slug: string, fields: string[] = []): PostItems {
+function checkForPopMdx(fullPath: string): boolean {
+  return /\\pop.mdx$/.test(fullPath);
+}
+
+function getPostHelperFunction(posts: PostItems[]) {
+  return posts
+    .filter((post) => !!post)
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+}
+
+function sanitizeFields(ctx: {
+  fields: string[];
+  fullPath: string;
+  slug: string;
+}) {
+  const { fields, fullPath, slug } = ctx;
+  const items: PostItems = {};
   const realSlug = slug.replace(/\.mdx$/, '');
-  // eslint-disable-next-line no-console
-  console.log(POSTS_PATH);
-  const fullPath = join(POSTS_PATH, `${realSlug}.mdx`);
+
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
 
-  const items: PostItems = {};
-
-  // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
     if (field === 'slug') {
       items[field] = realSlug;
@@ -33,15 +40,41 @@ export function getPostBySlug(slug: string, fields: string[] = []): PostItems {
       items[field] = data[field];
     }
   });
-
   return items;
+}
+
+export function getPostSlugs(): string[] {
+  return fs.readdirSync(POSTS_PATH);
+}
+
+export function getPopPostSlugs(): string[] {
+  return fs.readdirSync(POP_POSTS_PATH);
+}
+
+export function getPostBySlug(slug: string, fields: string[] = []): PostItems {
+  const realSlug = slug.replace(/\.mdx$/, '');
+  const fullPath = join(POSTS_PATH, `${realSlug}.mdx`);
+  if (checkForPopMdx(fullPath)) return null;
+  return sanitizeFields({ fields, fullPath, slug });
+}
+
+export function getPopPostBySlug(
+  slug: string,
+  fields: string[] = []
+): PostItems {
+  const realSlug = slug.replace(/\.mdx$/, '');
+  const fullPath = join(POP_POSTS_PATH, `${realSlug}.mdx`);
+  return sanitizeFields({ fields, fullPath, slug });
 }
 
 export function getAllPosts(fields: string[] = []): PostItems[] {
   const slugs = getPostSlugs();
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug, fields))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+  const posts = slugs.map((slug) => getPostBySlug(slug, fields));
+  return getPostHelperFunction(posts);
+}
+
+export function getAllPopPosts(fields: string[] = []): PostItems[] {
+  const slugs = getPopPostSlugs();
+  const posts = slugs.map((slug) => getPopPostBySlug(slug, fields));
+  return getPostHelperFunction(posts);
 }
