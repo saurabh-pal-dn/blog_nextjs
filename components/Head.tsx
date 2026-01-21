8ddde756e422467f36d744b856c1e3abb47d126d
“Saurabh <bestpal97@gmail.com>
2026-01-21 15:53:59 +0530

    upgrading to next 16.1.4

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,6 +1,6 @@
 import NextHead from 'next/head';
 import { useRouter } from 'next/router';
-import React from 'react';
+import { JSX } from 'react';
 import { MetaProps } from '../types/layout';
 
 export const WEBSITE_HOST_URL = 'https://blog-nextjs-tau-bice.vercel.app';
